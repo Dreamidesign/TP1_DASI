d0d110a20220b001325344b3e78bb6d11e7cbd94
Dreami_ <swyx3r@gmail.com>
2019-03-24 22:38:43 +0100

    Ajout de plein de nouveaux tests

--- src/main/java/vue/Test.java
+++ src/main/java/vue/Test.java
@@ -24,7 +24,8 @@ public class Test {
         s.initEmploye();
 
         DebugLogger.log("Inscription d'un client");
-        s.inscrireClient(new Client("Nathan","J", "Mr", "22/08/2004", "6 rue des Lilas, Lyon", "0658763255", "nathan@gmail.com", "margaux"));
+        s.inscrireClient(new Client("Nathan","J", "Mr", "22/08/2004", "6 rue " +
+                "des Lilas, Lyon", "0658763255", "nathan@gmail.com", "margaux"));
 
         DebugLogger.log("Connexion...");
         Client current = s.connexionClient("nathan@gmail.com", "margaux");
@@ -46,12 +47,12 @@ public class Test {
         for(Intervention inte : s.getInterventionJour(ii.getEmploye(), new Date()))
             System.out.println(inte);
 
-        DebugLogger.log("Deconnexion");
+        DebugLogger.log("Deconnexion...");
         current = null;
         DebugLogger.log("Inscription d'un autre client");
         s.inscrireClient(
-                new Client("Margaux","P", "Mme", "07/08/2010", "5 avenue albert einstein, Villeurbanne",
-                        "0658889900", "maca@gmail.com", "aedi"));
+                new Client("Margaux","P", "Mme", "07/08/2010", "5 avenue albert " +
+                        "einstein, Villeurbanne", "0658889900", "maca@gmail.com", "aedi"));
 
         DebugLogger.log("Test de connexion avec un mot de passe incorrect");
         current = s.connexionClient("maca@gmail.com", "abc");
@@ -85,18 +86,63 @@ public class Test {
         DebugLogger.log("Intervention en cours de l'employé de Villeurbanne");
         System.out.println(s.getInterventionAct(i3.getEmploye()));
 
+        DebugLogger.log("On ajoute encore 4 interventions pour occuper tous les employés");
+        Intervention tabInterv[] = {
+                new Incident("Commande de caviar"),
+                new Incident("Besoin de shampoing"),
+                new Animal("Chouquette","-"),
+                new Livraison("colis", "UPS","Laisser sur le pallier"),
+        };
+
+        for(Intervention iii : tabInterv)
+        {
+            s.demandeIntervention(current, iii);
+            iii = s.getInterventionAct(i.getEmploye());
+        }
+
+        DebugLogger.log("Ajout d'une intervention supplémentaire");
+        Intervention i5 = new Incident("Repassage costume");
+        s.demandeIntervention(current, i5);
+
+        DebugLogger.log("Historique d'intervention du client");
+        for(Intervention inter : s.getInterventionsClient(current))
+            System.out.println(inter);
+
+        DebugLogger.log("Deconnexion...");
+
+        DebugLogger.log("Connexion en tant qu'employé");
+        Employe currentE = s.connexionEmploye("insa@bg.bg", "insa");
+
+        DebugLogger.log("Récupération de l'intervention en cours de l'employé");
+        Intervention currentI = s.getInterventionAct(currentE);
+
+        DebugLogger.log("Validation de l'intervention");
+        s.validerIntervention(currentI, "RAS");
+
+        DebugLogger.log("Deconnexion...");
+        currentE = null;
+
+        DebugLogger.log("Connexion en tant qu'employé");
+        currentE = s.connexionEmploye("hollande@bg.bg", "macaron");
+
+        DebugLogger.log("Récupération de l'intervention en cours de l'employé");
+        currentI = s.getInterventionAct(currentE);
+
+        DebugLogger.log("Validation de l'intervention");
+        s.echecIntervention(currentI, "Inaccessible");
+
+        DebugLogger.log("Historique d'intervention du client");
+        for(Intervention inter : s.getInterventionsClient(current))
+            System.out.println(inter);
+
     }
     
        
 
     public static void main(String[] args){
-        
        JpaUtil.init();
-      
        Service s = new Service();
-
        testFonctionnement(s);
-
        JpaUtil.destroy();
     }
 
